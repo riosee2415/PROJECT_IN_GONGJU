57cca1672b4847d0cf0efee6e08fe884a35ef3fe
rlarudals <gim181004@gmail.com>
2020-07-15 01:33:55 +0900

    todo design

--- A-TEAM/calender-client/src/App.js
+++ A-TEAM/calender-client/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import "../css/styles.css";
+import DateAndTimePickers from "./components/DateAndTimePickers";
 
 class App extends React.Component {
   render() {
-    return <div>Hello!</div>;
+    return (
+      <div className="container">
+        <div className="calender"></div>
+        <div className="todo">
+          <div className="seleteDate">
+            <DateAndTimePickers />
+          </div>
+          <input type="text" className="todoList"></input>
+          <button className="check">check</button>
+        </div>
+      </div>
+    );
   }
 }
 
