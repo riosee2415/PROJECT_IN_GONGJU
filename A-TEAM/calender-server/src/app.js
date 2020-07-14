f326a7eb6dd57238196aa419668ce6b6496130fa
hyj11123 <rpgitemk1234@naver.com>
2020-07-14 20:09:31 +0900

    200714 1th

--- A-TEAM/calender-server/src/app.js
+++ A-TEAM/calender-server/src/app.js
@@ -9,18 +9,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-app.post("/api/test", (req, res) => {
-    console.log("Server Is Called By Client");
-
-    const {
-        body:{
-            params:{inputData}
-        }
-    } = req;
-
-    console.log(inputData);
-})
-
 app.listen(PORT, () => {
     console.log(`${PORT} server start`);
 });
