9e8e5a42958e6f7ab69fc807a176a48329ea2065
sts2003 <44109148+sts2003@users.noreply.github.com>
2020-07-20 23:00:20 +0900

    calendar api complete

--- B-TEAM/src/App.js
+++ B-TEAM/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CSS/styles.css";
+import Calendar from "../src/Calendar";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,30 +11,36 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <section className="App__top"></section>
+        <div className="App__top">
+          <Calendar size="xs" />
+        </div>
         <section className="App__bottom">
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button onClick={this._alertBtnHandler}> 확인 </button>
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button onClick={this._alertBtnHandler}> 확인 </button>
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button onClick={this._alertBtnHandler}> 확인 </button>
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button onClick={this._alertBtnHandler}> 확인 </button>
           </div>
         </section>
       </div>
     );
   }
+
+  _alertBtnHandler = () => {
+    alert("확인");
+  };
 }
 export default App;
