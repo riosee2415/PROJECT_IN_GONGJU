6b3ec36572c317f9c18f323814c7c55d8fd992b0
YERIM <dnaer456@gmail.com>
2020-07-20 23:01:22 +0900

    css

--- B-TEAM/src/App.js
+++ B-TEAM/src/App.js
@@ -11,25 +11,30 @@ class App extends React.Component {
     return (
       <div className="App">
         <section className="App__top"></section>
+
+        <section className="App__middle">
+          <button>확인</button>
+        </section>
+
         <section className="App__bottom">
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button> 삭제 </button>
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button> 삭제 </button>
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button> 삭제 </button>
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-            <button> 확인 </button>
+            <button> 삭제 </button>
           </div>
         </section>
       </div>
