e4d17e39431c10b9493fc808c784bc6fc5757fc2
qkdgywls0503 <qkdgywls0503@naver.com>
2020-07-20 18:43:37 +0900

    calenderTest-failed

---
+++ A-TEAM/calender-client/src/components/onPanelChange.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Calendar } from "antd";
+
+// function onPanelChange(value, mode) {
+//   console.log(value.format("YYYY-MM-DD"), mode);
+// }
+
+// ReactDOM.render(
+//   <Calendar onPanelChange={onPanelChange} />,
+//   document.getElementById("container")
+// );
