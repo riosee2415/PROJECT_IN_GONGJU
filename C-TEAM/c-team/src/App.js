76dfe5594fc9e062b64e0082e2cf76bfedfa5e9d
ouhouh1936 <ouhouh1936@gamil.com>
2020-07-21 22:06:09 +0900

    conf

---
+++ C-TEAM/c-team/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="todoApp1">
+        <input type="data" />
+      </div>
+      <div className="todoApp2"></div>
+      <div className="todoApp3"></div>
+      <div className="todoApp4"></div>
+    </div>
+  );
+}
+
+export default App;
