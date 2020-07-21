df24e707b8fad3e5c2a6ebf1bbcc21d511b5618a
YERIM <dnaer456@gmail.com>
2020-07-21 13:12:27 +0900

    again

--- B-TEAM/src/App.js
+++ B-TEAM/src/App.js
@@ -11,18 +11,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        <section className="App__top"></section>
-
-        <section className="App__middle">
-          <button>확인</button>
-        </section>
-
-        <section className="App__bottom">
-          <div className="App__bottom__box">
-            <input type="text" />
-            <button> 삭제 </button>
-=======
         <div className="App__top">
           <Calendar size="xs" />
         </div>
@@ -30,34 +18,21 @@ class App extends React.Component {
           <div className="App__bottom__box">
             <input type="text" />
             <button onClick={this._alertBtnHandler}> 확인 </button>
->>>>>>> 9e8e5a42958e6f7ab69fc807a176a48329ea2065
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-<<<<<<< HEAD
-            <button> 삭제 </button>
-=======
             <button onClick={this._alertBtnHandler}> 확인 </button>
->>>>>>> 9e8e5a42958e6f7ab69fc807a176a48329ea2065
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-<<<<<<< HEAD
-            <button> 삭제 </button>
-=======
             <button onClick={this._alertBtnHandler}> 확인 </button>
->>>>>>> 9e8e5a42958e6f7ab69fc807a176a48329ea2065
           </div>
 
           <div className="App__bottom__box">
             <input type="text" />
-<<<<<<< HEAD
-            <button> 삭제 </button>
-=======
             <button onClick={this._alertBtnHandler}> 확인 </button>
->>>>>>> 9e8e5a42958e6f7ab69fc807a176a48329ea2065
           </div>
         </section>
       </div>
